55b5e272d11ce78e534058f0e7f933a804f36b38
gigaherz <gigaherz@9ff8c5e4-9a92-11de-a77c-817936965df5>
2011-09-19 00:25:29 +0000

    Been testing the requirements to support Windows 7 Taskbar Jump Lists. I'm not sure I want to depend on the Windows API Code Pack though, because of licensing. I may need to implement the thing myself using COM interop. Added the ability to register itself as .txt file handler. Does not make itself the default  handler though, you have to choose it yourself.

--- SimpleTextEditor/Program.cs
+++ SimpleTextEditor/Program.cs
@@ -12,9 +12,46 @@ namespace SimpleTextEditor
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TextEditorWindow(args));
+            bool registration = false;
+            bool unregister = false;
+            bool userSpecific = false;
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "/RegisterUser")
+                {
+                    registration = true;
+                    userSpecific = true;
+                }
+                else if (args[0] == "/RegisterSystem")
+                {
+                    registration = true;
+                    userSpecific = true;
+                }
+                else if (args[0] == "/UnregisterUser")
+                {
+                    registration = true;
+                    unregister = true;
+                    userSpecific = true;
+                }
+                else if (args[0] == "/UnregisterSystem")
+                {
+                    registration = true;
+                    unregister = true;
+                    userSpecific = true;
+                }
+            }
+
+            if (registration)
+            {
+                FileAssociationTools.HandleFileAssociationRegistration_Elevated(unregister, userSpecific);
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new TextEditorWindow(args));
+            }
         }
     }
 }
